3331d947599d2bc6b6a113cbf926a83091c3dfdc
Mumis <emil.wertwein@gmail.com>
2021-05-17 20:59:46 +0200

    Implement logout functionality & add animation for authform

--- src/common/header.tsx
+++ src/common/header.tsx
@@ -1,73 +1,30 @@
 import { Link } from 'react-router-dom';
-import React, { Component, ReactNode } from 'react';
-import { observer } from 'mobx-react';
-import { resolve } from 'inversify-react';
-import NotificationsSharpIcon from '@material-ui/icons/NotificationsSharp';
+import React, { FC } from 'react';
 import ExpandMoreSharpIcon from '@material-ui/icons/ExpandMoreSharp';
-import ChatBubbleSharpIcon from '@material-ui/icons/ChatBubbleSharp';
-import { Bind } from '@decorize/bind';
-import DefaultProfileImage from '@/images/DefaultProfileImage.png';
-import { StandardLinkBtn } from '@/common/standard-link-btn';
+import { UserMenu } from '@/common/user-menu';
 import styles from '@/common/header.scss';
-import { User, YummeClient, YUMME_CLIENT_TYPE } from '@/api/yumme-client';
 
-interface HeaderState {
-    currentUser?: User;
-}
+export const Header: FC<unknown> = () => (
+    <header className={ styles.header }>
+        <div className={ styles.content }>
+            <div className={ styles.left }>
+                <Link to="/">
+                    <span className={ styles.logo }>Yumme</span>
+                </Link>
 
-@observer
-export class Header extends Component<unknown, HeaderState> {
-    @resolve(YUMME_CLIENT_TYPE)
-    private readonly yummeClient: YummeClient;
-
-    public constructor(props: unknown) {
-        super(props);
-
-        this.state = {};
-    }
-
-    public componentDidMount(): void {
-        this.onRefresh();
-    }
-
-    public render(): ReactNode {
-        return (
-            <header className={ styles.header }>
-                <div className={ styles.content }>
-                    <div className={ styles.left }>
-                        <Link to="/">
-                            <span className={ styles.logo }>Yumme</span>
-                        </Link>
-
-                        <nav>
-                            <ul>
-                                <li>
-                                    Browse
-                                    { ' ' }
-                                    <ExpandMoreSharpIcon />
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div className={ styles.right }>
-                        <StandardLinkBtn path="/recipe/new">+ ADD RECIPE</StandardLinkBtn>
-                        <ChatBubbleSharpIcon />
-                        <NotificationsSharpIcon />
-                        <Link to={ `/profile/${ this.state.currentUser?.id ?? 1 }` }>
-                            <img
-                                className={ styles.profile }
-                                src={ DefaultProfileImage } />
-                        </Link>
-                    </div>
-                </div>
-            </header>
-        );
-    }
-
-    @Bind
-    private async onRefresh(): Promise<void> {
-        const currentUser = await this.yummeClient.getCurrentUser();
-
-        this.setState({ currentUser });
-    }
-}
+                <nav>
+                    <ul>
+                        <li>
+                            Browse
+                            { ' ' }
+                            <ExpandMoreSharpIcon />
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className={ styles.right }>
+                <UserMenu />
+            </div>
+        </div>
+    </header>
+);
---
+++ src/common/user-menu.tsx
@@ -0,0 +1,79 @@
+import { Link } from 'react-router-dom';
+import React, { Component, ReactNode } from 'react';
+import { resolve } from 'inversify-react';
+import NotificationsSharpIcon from '@material-ui/icons/NotificationsSharp';
+import ExitToAppSharpIcon from '@material-ui/icons/ExitToAppSharp';
+import ChatBubbleSharpIcon from '@material-ui/icons/ChatBubbleSharp';
+import { bind } from '@decorize/bind';
+import DefaultProfileImage from '@/images/DefaultProfileImage.png';
+import styles from '@/common/user-menu.scss';
+import { AuthState } from '@/authentication/auth-state';
+import { User, YummeClient, YUMME_CLIENT_TYPE } from '@/api/yumme-client';
+import { StandardLinkBtn } from './standard-link-btn';
+
+interface UserMenuState {
+    currentUser?: User;
+}
+
+export class UserMenu extends Component<unknown, UserMenuState> {
+    @resolve(AuthState)
+    private readonly authState: AuthState;
+
+    @resolve(YUMME_CLIENT_TYPE)
+    private readonly yummeClient: YummeClient;
+
+    public constructor(props: unknown) {
+        super(props);
+
+        this.state = {};
+    }
+
+    public componentDidMount(): void {
+        this.onRefresh();
+    }
+
+    public render(): ReactNode {
+        return (
+            <ul className={ styles.userMenu }>
+                <li className={ styles.navigationItem }>
+                    <StandardLinkBtn path="/recipe/new">+ ADD RECIPE</StandardLinkBtn>
+                </li>
+                <li className={ styles.navigationItem }>
+                    <ChatBubbleSharpIcon className={ styles.icon } />
+                </li>
+                <li className={ styles.navigationItem }>
+                    <NotificationsSharpIcon className={ styles.icon } />
+                </li>
+                <li className={ styles.navigationItem }>
+                    <Link to={ `/profile/${ this.state.currentUser?.id ?? 1 }` }>
+                        <img
+                        className={ styles.profileImage }
+                        src={ DefaultProfileImage } />
+                    </Link>
+
+                    <ul className={ styles.dropdown }>
+                        <li className={ styles.menuItem }>
+                            <Link to={ `/profile/${ this.state.currentUser?.id ?? 1 }` }>
+                                <span>Go to my profile</span>
+                            </Link>
+                        </li>
+
+                        <li className={ styles.menuItem } onClick={ (): void => this.authState.logout() }>
+                            <Link to="">
+                                <span>Log out</span>
+                                <ExitToAppSharpIcon />
+                            </Link>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        );
+    }
+
+    @bind
+    private async onRefresh(): Promise<void> {
+        const currentUser = await this.yummeClient.getCurrentUser();
+
+        this.setState({ currentUser });
+    }
+}
